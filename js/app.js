1d11b9d92b8c2d002fcac1d052c482997d5d96cd
Yasser A. Gamal <63634166+yasser-skiller@users.noreply.github.com>
2020-12-20 15:29:48 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ var swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   spaceBetween: 30,
   // loop: true,
-  // init: false,
+  init: false,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
