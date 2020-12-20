c4ff0e3d9f1c06eccda9a415ff7c433caa34e2b3
Yasser A. Gamal <63634166+yasser-skiller@users.noreply.github.com>
2020-12-20 13:42:21 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -10,3 +10,37 @@ arr_active_link.forEach(element => {
   })
 });
 
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // init: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    // 1024: {
+    //   slidesPerView: 5,
+    //   spaceBetween: 50,
+    // },
+  }
+});
+
+// var swiper = new Swiper('.swiper-container', {
+//   slidesPerView: 4,
+//   centeredSlides: true,
+//   spaceBetween: 30,
+//   grabCursor: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
