7c51479014b43cf96cda1dc802f5ffb2210dca56
Yasser A. Gamal <63634166+yasser-skiller@users.noreply.github.com>
2020-12-20 15:57:07 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ arr_active_link.forEach(element => {
 });
 
 var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 30,
   slidesPerGroup: 1,
   // loop: true,
